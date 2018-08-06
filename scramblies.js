3419fd1d7fb3aded384e894243c5b09e11ccf318
Manuel <m.a.c.p90@gmail.com>
2018-08-06 10:29:22 -0400

    almos done only failing the 3er test

---
+++ scramblies.js
@@ -0,0 +1,35 @@
+function scramble(str1, str2) {
+  const finalarray = [];
+
+  //spliting both arrays / and sorting them
+  let toCompare = str2.split("");
+  let toGet = str1.split("");
+
+  //nesting two forEach to compare each element of esch array to retuen a single array with the element that match
+  toCompare.forEach(elementTocCompare => {
+    toGet.forEach(elementToGet => {
+      if (elementTocCompare === elementToGet) {
+        toGet.splice(elementToGet, 1);
+        finalarray.push(elementToGet);
+      }
+      //   if (elementTocCompare !== elementToGet) {
+      //     toCompare.splice(elementTocCompare, 1);
+      //   }
+    });
+  });
+  console.log(toCompare);
+  console.log(finalarray);
+  //   compare the lenght of the arrays
+  if (finalarray.length >= toCompare.length) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+// scramble("jscripts", "javascript");
+//false
+scramble("katas", "steak");
+//true
+
+// [ stak' ]
