302659d4c5e1517d9840b40c9997ffaaa7c3f179
Manuel <m.a.c.p90@gmail.com>
2018-08-11 12:13:14 -0400

    refactoring old code

---
+++ WhosSnline.js
@@ -0,0 +1,22 @@
+const whosOnline = friends => {};
+
+// array to test
+friends = [
+  {
+    username: "David",
+    status: "online",
+    lastActivity: 10
+  },
+  {
+    username: "Lucy",
+    status: "offline",
+    lastActivity: 22
+  },
+  {
+    username: "Bob",
+    status: "online",
+    lastActivity: 104
+  }
+];
+
+whosOnline(friends);
