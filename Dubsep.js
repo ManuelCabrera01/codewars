e768dd23cc6dfd1ba0afd0c3e00ea4d79855132c
Manuel <m.a.c.p90@gmail.com>
2018-08-22 19:58:31 -0400

    dubstep

--- Dubsep.js
+++ Dubsep.js
@@ -1,1 +1,10 @@
-function songDecoder(song) {}
+// objects literal
+function songDecoder(song) {
+  var a = song.split("WUB");
+  for (var i = a.length; i >= 0; i--) {
+    if (a[i] === "") {
+      a.splice(i, 1);
+    }
+  }
+  return a.join(" ");
+}
