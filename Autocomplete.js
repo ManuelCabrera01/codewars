bc603499d0a1792b3b4869dccbf93faaab0e5792
Manuel <m.a.c.p90@gmail.com>
2018-08-25 12:15:56 -0400

    tribonacci work

--- Autocomplete.js
+++ Autocomplete.js
@@ -1,21 +1,20 @@
-var one= 'Ai'
-var array =  ['airpla$#%#$ne','aIrp@###ort','ap[]][ple','ball']
+var one = "Ai";
+var array = ["airpla$#%#$ne", "aIrp@###ort", "ap[]][ple", "ball"];
 
+let autocomplete = (input, dictionary) => {
+  let matches = [];
+  let firstRegex = new RegExp(/[^A-Za-z]/g);
+  let secondRegex = new RegExp(input, "gi");
 
-let autocomplete =((input, dictionary) =>{
-let matches = []
-let firstRegex = new RegExp(/[^A-Za-z]/g)
-let secondRegex =new RegExp( input,'gi')
+  //cleaning theincput array  from any signbol
+  let cleanARray = dictionary.map(dirtyW => {
+    return dirtyW.replace(firstRegex, "");
+  });
 
-//cleaning theincput array  from any signbol
-let cleanARray = dictionary.map((dirtyW)=>{
-  return dirtyW.replace(firstRegex, "") 
-   })
-
-cleanARray.forEach((element)=>{
-matches.push(element.match(secondRegex))
-})
-console.log(matches)
-
-}) 
-autocomplete(one, array)
+  cleanARray.forEach(element => {
+    var origin = element.match(secondRegex);
+    matches.push(element.match(`${secondRegex}`));
+  });
+  console.log(matches);
+};
+autocomplete(one, array);
--- TribonacciSequence.js
+++ TribonacciSequence.js
@@ -1,3 +1,25 @@
 function tribonacci(signature, n) {
-  //your code here
+  let arr = signature;
+  if (n === 0) {
+    return [];
+  } else if (n === 1) {
+    return [1];
+  } else {
+    let a = signature[0];
+    let b = signature[1];
+    let c = signature[2];
+    let next = c;
+
+    for (let i = 1; i <= n - 3; i++) {
+      next = a + b + c;
+      a = b;
+      b = c;
+      c = next;
+      arr.push(next);
+    }
+  }
+  return arr;
 }
+
+let myarr = [1, 2, 3];
+tribonacci(myarr, 1);
